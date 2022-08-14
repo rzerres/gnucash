3d0906e2d3bdf3c31f5c59f8d15364ff9cd88e13
Ralf Zerres <ralf.zerres@mail.de>
2022-08-14 15:34:04 +0200

    [gnucash/gnome] update dialog-distriblists
    * prepare treeview, where owner objects are selectable.
      This objects  (e.g. coowner, employees) are considered valid targets, when calculating
      apportionable values advertised in settlements.
    
    Signed-off-by: Ralf Zerres <ralf.zerres@mail.de>

--- gnucash/gnome/dialog-distriblists.h
+++ gnucash/gnome/dialog-distriblists.h
@@ -24,8 +24,12 @@
     @{ */
 /** @addtogroup CoOwner
 
-This file implements the UI dialogs needed to manage ::Distribution Lists
-objects.
+This file implements the UI dialogs needed to manage ::Distribution Lists.
+
+Distribution lists offer the possibility to define reusable rules,
+to calculate split values for assigend owner objects. Split values
+are useful in reports (e.g settlements) where the distribution list rules
+are consumed to calculate the proportional value for each assigned owner object.
 
 The UI is designed to group suitable informations in frames.
 
@@ -34,22 +38,20 @@ The UI is designed to group suitable informations in frames.
    -# Description
    -# List type
 
-Right now, we handle just one type: Co-Ownership shares.
-You may create, edit or delete entities.
+You may create, edit or delete distribution list entities.
 
-A list is used to define attributes that are needed, when account
-balances are to be split proportionally into co-owner settlements.
+Within a list you define a list type. Each list type will hold the
+100% share value (the numerator). The numerator is an absolute or
+a percentage value.
 
-Each list defines the 100% share value (the numerator) as the
-calculation base. You assign the appropriate unit share value inside
-the co-owners objects, which in term is referenced to calculate the
-costs to be settled to this apartment unit (the devisor).
+Assigned owner objects will offer their appropriate unit share value
+as an object attribute. To calculate the settlement costs, the share
+value is referenced as the devisor.
 
-Example:
-Calculation base with 100% = 1000 units
-Apartment unit 1 with 10% = 100 shares
-Co-ownership share = 1000/100 * account balance
-
+Co-Owner example:
+Calculation base with 100% = 1000 units (numerator)
+Apartment unit 1 with 10% = 100 shares (devisor)
+Co-Owner settlement value = 1000/100 * account balance (owner object)
     @{ */
 /** @file dialog-distriblists.h
     @brief handling distribution lists dialog objects
@@ -61,14 +63,17 @@ Co-ownership share = 1000/100 * account balance
 
 /** @struct DistributionListsWindow
 
-@param  GtkWidget*  window - Pointer to the window The instance entity.
-@param  GtkWidget*  entry_description - The description of the distributon list.
+@param  GtkWidget*  window - Pointer to the window.
+@param  GtkWidget*  label_definition - The window label string for the distribution list.
+@param  GtkWidget*  label_description - The label string of the distribution list.
+@param  GtkWidget*  entry_description - The description of the distribution list.
 @param  GtkWidget*  label_type - Label of the distribution list type.
 @param  GtkWidget*  entry_type - The type of a distribution list.
 @param  GtkWidget*  vbox - The frame that handles distribution lists.
-@param  GtkWidget*  view_lists - The frame that presents available distribution lists.
 @param  GtkWidget*  vbox_definitions - The frame that presents details assigned to the active distribution list.
+@param  GtkWidget*  view_lists - The frame that presents available distribution lists.
 @param  DistributionListNotebook - notebook The box that presents the attributes of a distribution list.
+@param  DistributionListOwners - owners The box that presents the owner specific attributes of a distribution list.
 @param  GncDistributionList* current_list - Copy of the current distribution list pointer.
 @param  QofBook* book - Copy of the book pointer.
 @param  gint component_id - A unique component identier.
@@ -76,15 +81,86 @@ Co-ownership share = 1000/100 * account balance
 */
 typedef struct _distribution_lists_window DistributionListsWindow;
 
+/** @struct DistributionListsNotebook
+
+@param  GtkWidget*  dialog - Pointer to the notebook dialog.
+@param  GtkWidget*  notebook - Pointer to the notebook entry.
+@param  GtkWidget*  parent - Pointer to the parent of the notebook dialog.
+*/
+typedef struct _distribution_lists_notebook DistributionListsNotebook;
+
+/** @struct DistributionListsOwners
+
+@param  GtkWidget*  dialog - Pointer to the owners dialog.
+@param  GtkWidget*  owners - Pointer to the owners entry.
+@param  GtkWidget*  parent - Pointer to the parent of the owners dialog.
+*/
+typedef struct _distribution_lists_owners DistributionListsOwners;
+
 #include "qof.h"
 
 /** @name Create/Destroy window Functions
  @{ */
+
+/** Create a new DistribuionList window that list the available
+ *distribution list objects.
+ *
+ * It offers callbacks to create, modify or delete lists objects. */
 DistributionListsWindow
 *gnc_ui_distriblists_window_new (GtkWindow *parent, QofBook *book);
+/** @} */
 
 /* Destroy a distribution lists window */
 //void
 //gnc_ui_distriblists_window_destroy (DistriblistsWindow *ttw);
 
+/** @name Callback Functions
+ @{ */
+void
+distriblists_list_delete_cb (
+    GtkButton *button,
+    DistributionListsWindow *distriblists_window);
+void
+distriblists_list_edit_cb (
+    GtkButton *button,
+    DistributionListsWindow *distriblists_window);
+void
+distriblists_list_new_cb (
+    GtkButton *button,
+    DistributionListsWindow *distriblists_window);
+void
+distriblists_type_changed_cb (
+    GtkComboBox *combobox,
+    gpointer data);
+void
+distriblists_window_close_cb (
+    GtkWidget *widget,
+    gpointer data);
+void
+distriblists_window_destroy_cb (
+    GtkWidget *widget,
+    gpointer data);
+gboolean
+distriblists_window_key_press_cb (
+    GtkWidget *widget, GdkEventKey *event,
+    gpointer data);
+void
+owners_assign_cb (
+    GtkButton *button,
+    DistributionListsWindow *distriblists_window);
+void
+owners_edit_cb (
+    GtkButton *button,
+    DistributionListsWindow *distriblists_window);
+void owners_ok_cb (
+    GtkButton *button,
+    DistributionListsWindow *distriblists_window);
+void
+owners_remove_cb (
+    GtkButton *button,
+    DistributionListsWindow *distriblists_window);
+/** @} */
+
 #endif /* _DIALOG_DISTRIBUTION_LISTS_H */
+/** @} */
+/** @} */
