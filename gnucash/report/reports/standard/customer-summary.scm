526e7fbf46fdd9e8f3aba52aa3eb5bc08b55b94e
Ralf Zerres <ralf.zerres@mail.de>
2023-04-15 18:04:21 +0200

    [gnucash/report/reports/standard] update customer-summary.scm
    Signed-off-by: Ralf Zerres <ralf.zerres@mail.de>

--- gnucash/report/reports/standard/customer-summary.scm
+++ gnucash/report/reports/standard/customer-summary.scm
@@ -4,7 +4,7 @@
 ;; Created by:  Christian Stimming
 ;; Copyright (c) 2010 Christian Stimming <christian@cstimming.de>
 ;; Copyright (c) 2002, 2003 Derek Atkins <warlord@MIT.EDU>
-;;q
+;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation; either version 2 of
