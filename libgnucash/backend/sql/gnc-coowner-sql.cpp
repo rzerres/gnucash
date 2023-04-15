dd2da49ee6b66e0031e1ecb6d67500ecc0bd6076
Ralf Zerres <ralf.zerres@mail.de>
2023-04-15 18:04:04 +0200

    [libgnucash/backend/sql] introduce Co-Owner handling
    * CMakeLists: add header and sources
    * gnc-coowner-sql header
    * gnc-coowner-sqo source
    * gnc-customer-sql bugfix
    
    Signed-off-by: Ralf Zerres <ralf.zerres@mail.de>

--- libgnucash/backend/sql/gnc-coowner-sql.cpp
+++ libgnucash/backend/sql/gnc-coowner-sql.cpp
@@ -1,5 +1,5 @@
 /********************************************************************\
- * gnc-customer-sql.c -- customer sql backend                       *
+ * gnc-coowner-sql.c -- co-owner sql implementation                 *
  *                                                                  *
  * This program is free software; you can redistribute it and/or    *
  * modify it under the terms of the GNU General Public License as   *
@@ -20,110 +20,116 @@
  *                                                                  *
 \********************************************************************/
 
-/** @file gnc-customer-sql.c
+/** @file gnc-coowner-sql.c
  *  @brief load and save address data to SQL
- *  @author Copyright (c) 2007-2008 Phil Longstaff <plongstaff@rogers.com>
+ *  @author Copyright (c) 2022 Ralf Zerres <ralf.zerres@mail.de>
  *
  * This file implements the top-level QofBackend API for saving/
  * restoring data to/from an SQL database
  */
 #include <glib.h>
 
+extern "C"
+{
 #include <config.h>
 
 #include <stdlib.h>
 #include <string.h>
 
+#include "gnc-commodity.h"
 #include "gncBillTermP.h"
-#include "gncCustomerP.h"
+#include "gncCoOwnerP.h"
 #include "gncTaxTableP.h"
+}
+
+#include "gnc-bill-term-sql.h"
+#include "gnc-commodity-sql.h"
+#include "gnc-coowner-sql.h"
+#include "gnc-slots-sql.h"
+#include "gnc-tax-table-sql.h"
 
 #include "gnc-sql-connection.hpp"
 #include "gnc-sql-backend.hpp"
 #include "gnc-sql-object-backend.hpp"
 #include "gnc-sql-column-table-entry.hpp"
-#include "gnc-slots-sql.h"
-#include "gnc-customer-sql.h"
-#include "gnc-bill-term-sql.h"
-#include "gnc-tax-table-sql.h"
 
-#define _GNC_MOD_NAME   GNC_ID_CUSTOMER
+#define _GNC_MOD_NAME   GNC_ID_COOWNER
 
 static QofLogModule log_module = G_LOG_DOMAIN;
 
-#define TABLE_NAME "customers"
-#define TABLE_VERSION 2
-
-#define MAX_NAME_LEN 2048
+#define MAX_ACL_LEN 2048
 #define MAX_ID_LEN 2048
-#define MAX_NOTES_LEN 2048
+#define MAX_LANGUAGE_LEN 2048
+#define MAX_NAME_LEN 2048
+#define MAX_USERNAME_LEN 2048
+
+#define TABLE_NAME "coowner"
+#define TABLE_VERSION 2
 
 static EntryVec col_table
 ({
-    gnc_sql_make_table_entry<CT_GUID>("guid", 0, COL_NNUL | COL_PKEY, "guid" ),
+    gnc_sql_make_table_entry<CT_STRING>("acl", MAX_ACL_LEN, COL_NNUL, "acl"),
+    gnc_sql_make_table_entry<CT_BOOLEAN>("active", 0, COL_NNUL, "active"),
+    gnc_sql_make_table_entry<CT_ADDRESS>("addr", 0, 0, "address"),
+    gnc_sql_make_table_entry<CT_NUMERIC>("apt_share", 0, COL_NNUL, "apt_share"),
+    gnc_sql_make_table_entry<CT_NUMERIC>("apt_unit", 0, COL_NNUL, "apt_unit"),
+    gnc_sql_make_table_entry<CT_ACCOUNTREF>(
+        "ccard_guid", 0, 0, "credit-card-account"),
+    gnc_sql_make_table_entry<CT_COMMODITYREF>(
+        "currency", 0, COL_NNUL, "currency"),
+    gnc_sql_make_table_entry<CT_NUMERIC>(
+        "distribution_key", 0, COL_NNUL, "distribution_key"),
+    gnc_sql_make_table_entry<CT_GUID>("guid", 0, COL_NNUL | COL_PKEY, "guid"),
+    gnc_sql_make_table_entry<CT_STRING>(
+        "language", MAX_LANGUAGE_LEN, COL_NNUL, "language"),
+    gnc_sql_make_table_entry<CT_STRING>("id", MAX_ID_LEN, COL_NNUL, "id"),
     gnc_sql_make_table_entry<CT_STRING>("name", MAX_NAME_LEN, COL_NNUL, "name"),
-    gnc_sql_make_table_entry<CT_STRING>("id", MAX_ID_LEN, COL_NNUL,
-                                        CUSTOMER_ID, true),
-    gnc_sql_make_table_entry<CT_STRING>("notes", MAX_NOTES_LEN, COL_NNUL,
-                                        CUSTOMER_NOTES, true),
-    gnc_sql_make_table_entry<CT_BOOLEAN>("active", 0, COL_NNUL,
-                                         QOF_PARAM_ACTIVE, true),
-    gnc_sql_make_table_entry<CT_NUMERIC>("discount", 0, COL_NNUL,
-                                         CUSTOMER_DISCOUNT, true),
-    gnc_sql_make_table_entry<CT_NUMERIC>("credit", 0, COL_NNUL,
-                                         CUSTOMER_CREDIT, true),
-    gnc_sql_make_table_entry<CT_COMMODITYREF>("currency", 0, COL_NNUL,
-                                         (QofAccessFunc)gncCustomerGetCurrency,
-                                         (QofSetterFunc)gncCustomerSetCurrency),
-    gnc_sql_make_table_entry<CT_BOOLEAN>("tax_override", 0, COL_NNUL,
-                                         CUSTOMER_TT_OVER, true),
-    gnc_sql_make_table_entry<CT_ADDRESS>("addr", 0, 0, CUSTOMER_ADDR,
-                                         true),
-    gnc_sql_make_table_entry<CT_ADDRESS>("shipaddr", 0, 0, CUSTOMER_SHIPADDR,
-                                         true),
-    gnc_sql_make_table_entry<CT_BILLTERMREF>("terms", 0, 0, CUSTOMER_TERMS,
-                                             true),
-    gnc_sql_make_table_entry<CT_INT>("tax_included", 0, 0,
-                                     (QofAccessFunc)gncCustomerGetTaxIncluded,
-                                     (QofSetterFunc)gncCustomerSetTaxIncluded),
-    gnc_sql_make_table_entry<CT_TAXTABLEREF>("taxtable", 0, 0,
-                                         (QofAccessFunc)gncCustomerGetTaxTable,
-                                         (QofSetterFunc)gncCustomerSetTaxTable),
+    gnc_sql_make_table_entry<CT_BOOLEAN>(
+        "tax_override", 0, COL_NNUL, COOWNER_TAXTABLE_OVERRIDE, true),
+    gnc_sql_make_table_entry<CT_STRING>(
+        "username", MAX_USERNAME_LEN, COL_NNUL, "username"),
+    gnc_sql_make_table_entry<CT_INT>(
+         "tax_included", 0, 0,
+         (QofAccessFunc)gncCoOwnerGetTaxIncluded,
+         (QofSetterFunc)gncCoOwnerSetTaxIncluded),
+    gnc_sql_make_table_entry<CT_TAXTABLEREF>(
+          "taxtable", 0, 0,
+          (QofAccessFunc)gncCoOwnerGetTaxTable,
+          (QofSetterFunc)gncCoOwnerSetTaxTable),
 });
 
-GncSqlCustomerBackend::GncSqlCustomerBackend() :
-    GncSqlObjectBackend(TABLE_VERSION, GNC_ID_CUSTOMER,
+GncSqlCoOwnerBackend::GncSqlCoOwnerBackend() :
+    GncSqlObjectBackend(TABLE_VERSION, GNC_ID_COOWNER,
                         TABLE_NAME, col_table) {}
 
-static GncCustomer*
-load_single_customer (GncSqlBackend* sql_be, GncSqlRow& row)
+static GncCoOwner*
+load_single_coowner (GncSqlBackend* sql_be, GncSqlRow& row)
 {
     const GncGUID* guid;
-    GncCustomer* pCustomer;
+    GncCoOwner* pCoOwner;
 
     g_return_val_if_fail (sql_be != NULL, NULL);
 
     guid = gnc_sql_load_guid (sql_be, row);
-    pCustomer = gncCustomerLookup (sql_be->book(), guid);
-    if (pCustomer == NULL)
+    pCoOwner= gncCoOwnerLookup (sql_be->book(), guid);
+    if (pCoOwner == NULL)
     {
-        pCustomer = gncCustomerCreate (sql_be->book());
+        pCoOwner = gncCoOwnerCreate (sql_be->book());
     }
-    gnc_sql_load_object (sql_be, row, GNC_ID_CUSTOMER, pCustomer, col_table);
-    qof_instance_mark_clean (QOF_INSTANCE (pCustomer));
+    gnc_sql_load_object (sql_be, row, GNC_ID_COOWNER, pCoOwner, col_table);
+    qof_instance_mark_clean (QOF_INSTANCE (pCoOwner));
 
-    return pCustomer;
+    return pCoOwner;
 }
-
-/* Because gncCustomerLookup has the arguments backwards: */
-static inline GncCustomer*
-gnc_customer_lookup (const GncGUID *guid, const QofBook *book)
+/* Because gncCoOwnerLookup has the arguments backwards: */
+static inline GncCoOwner*
+gnc_coowner_lookup (const GncGUID *guid, const QofBook *book)
 {
-     QOF_BOOK_RETURN_ENTITY(book, guid, GNC_ID_CUSTOMER, GncCustomer);
+    QOF_BOOK_RETURN_ENTITY(book, guid, GNC_ID_COOWNER, GncCoOwner);
 }
 
 void
-GncSqlCustomerBackend::load_all (GncSqlBackend* sql_be)
+GncSqlCoOwnerBackend::load_all (GncSqlBackend* sql_be)
 {
     g_return_if_fail (sql_be != NULL);
 
@@ -132,18 +138,19 @@ GncSqlCustomerBackend::load_all (GncSqlBackend* sql_be)
     auto result = sql_be->execute_select_statement(stmt);
 
     for (auto row : *result)
-        load_single_customer (sql_be, row);
+        load_single_coowner (sql_be, row);
 
     std::string pkey(col_table[0]->name());
     sql = "SELECT DISTINCT ";
     sql += pkey + " FROM " TABLE_NAME;
     gnc_sql_slots_load_for_sql_subquery (sql_be, sql,
-                                         (BookLookupFn)gnc_customer_lookup);
+                                         (BookLookupFn)gnc_coowner_lookup);
 }
+
 
 /* ================================================================= */
 void
-GncSqlCustomerBackend::create_tables (GncSqlBackend* sql_be)
+GncSqlCoOwnerBackend::create_tables (GncSqlBackend* sql_be)
 {
     gint version;
 
@@ -160,26 +167,26 @@ GncSqlCustomerBackend::create_tables (GncSqlBackend* sql_be)
         sql_be->upgrade_table(TABLE_NAME, col_table);
         sql_be->set_table_version (TABLE_NAME, TABLE_VERSION);
 
-        PINFO ("Customers table upgraded from version 1 to version %d\n",
+        PINFO ("CoOwners table upgraded from version 1 to version %d\n",
                TABLE_VERSION);
     }
 }
 
 /* ================================================================= */
 bool
-GncSqlCustomerBackend::commit (GncSqlBackend* sql_be, QofInstance* inst)
+GncSqlCoOwnerBackend::commit (GncSqlBackend* sql_be, QofInstance* inst)
 {
-    GncCustomer* customer;
+    GncCoOwner* coowner;
     const GncGUID* guid;
     E_DB_OPERATION op;
     gboolean is_infant;
     gboolean is_ok = TRUE;
 
     g_return_val_if_fail (inst != NULL, FALSE);
-    g_return_val_if_fail (GNC_IS_CUSTOMER (inst), FALSE);
+    g_return_val_if_fail (GNC_IS_COOWNER (inst), FALSE);
     g_return_val_if_fail (sql_be != NULL, FALSE);
 
-    customer = GNC_CUSTOMER (inst);
+    coowner = GNC_COOWNER (inst);
 
     is_infant = qof_instance_get_infant (inst);
     if (qof_instance_get_destroying (inst))
@@ -197,12 +204,12 @@ GncSqlCustomerBackend::commit (GncSqlBackend* sql_be, QofInstance* inst)
     if (op != OP_DB_DELETE)
     {
         // Ensure the commodity is in the db
-        is_ok = sql_be->save_commodity(gncCustomerGetCurrency (customer));
+        is_ok = sql_be->save_commodity(gncCoOwnerGetCurrency (coowner));
     }
 
     if (is_ok)
     {
-        is_ok = sql_be->do_db_operation(op, TABLE_NAME, GNC_ID_CUSTOMER, customer,
+        is_ok = sql_be->do_db_operation(op, TABLE_NAME, GNC_ID_COOWNER, coowner,
                                         col_table);
     }
 
@@ -225,14 +232,14 @@ GncSqlCustomerBackend::commit (GncSqlBackend* sql_be, QofInstance* inst)
 
 /* ================================================================= */
 static gboolean
-customer_should_be_saved (GncCustomer* customer)
+coowner_should_be_saved (GncCoOwner* coowner)
 {
     const char* id;
 
-    g_return_val_if_fail (customer != NULL, FALSE);
+    g_return_val_if_fail (coowner != NULL, FALSE);
 
-    /* Make sure this is a valid customer before we save it -- should have an ID */
-    id = gncCustomerGetID (customer);
+    /* make sure this is a valid coowner before we save it -- should have an ID */
+    id = gncCoOwnerGetID (coowner);
     if (id == NULL || *id == '\0')
     {
         return FALSE;
@@ -242,22 +249,22 @@ customer_should_be_saved (GncCustomer* customer)
 }
 
 static void
-write_single_customer (QofInstance* term_p, gpointer data_p)
+write_single_coowner (QofInstance* term_p, gpointer data_p)
 {
-    auto data = reinterpret_cast<write_objects_t*>(data_p);
+    write_objects_t* s = (write_objects_t*)data_p;
 
     g_return_if_fail (term_p != NULL);
-    g_return_if_fail (GNC_IS_CUSTOMER (term_p));
+    g_return_if_fail (GNC_IS_COOWNER (term_p));
     g_return_if_fail (data_p != NULL);
 
-    if (customer_should_be_saved (GNC_CUSTOMER (term_p)))
+    if (s->is_ok && coowner_should_be_saved (GNC_COOWNER (term_p)))
     {
-        data->commit (term_p);
+        s->is_ok = s->obe->commit (s->be, term_p);
     }
 }
 
 bool
-GncSqlCustomerBackend::write (GncSqlBackend* sql_be)
+GncSqlCoOwnerBackend::write (GncSqlBackend* sql_be)
 {
     write_objects_t data;
 
@@ -266,9 +273,10 @@ GncSqlCustomerBackend::write (GncSqlBackend* sql_be)
     data.be = sql_be;
     data.is_ok = TRUE;
     data.obe = this;
-    qof_object_foreach (GNC_ID_CUSTOMER, sql_be->book(), write_single_customer,
-                        (gpointer)&data);
+    qof_object_foreach (GNC_ID_COOWNER, sql_be->book(), write_single_coowner, &data);
+
     return data.is_ok;
 }
+
 
 /* ========================== END OF FILE ===================== */
