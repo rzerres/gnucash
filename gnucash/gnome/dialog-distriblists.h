23373b37ce44b15852aa4aa3605ada5d9ab40ad1
Ralf Zerres <ralf.zerres@mail.de>
2023-05-26 12:46:29 +0200

    dialog-distriblists: handle owners window

--- gnucash/gnome/dialog-distriblists.h
+++ gnucash/gnome/dialog-distriblists.h
@@ -75,7 +75,7 @@ Co-Owner settlement value = 1000/100 * account balance (owner object)
 @param  GtkWidget*  vbox_definitions - The frame that presents details assigned to the active distribution list.
 @param  GtkWidget*  view_lists - The frame that presents available distribution lists.
 @param  DistributionListNotebook - notebook The box that presents the attributes of a distribution list.
-@param  DistributionListOwners - owners The box that presents the owner specific attributes of a distribution list.
+@param  DistributionListOwners - owners The list of associated owners of the given distribution list.
 @param  GncDistributionList* current_list - Copy of the current distribution list pointer.
 @param  QofBook* book - Copy of the book pointer.
 @param  gint component_id - A unique component identier.
@@ -83,21 +83,40 @@ Co-Owner settlement value = 1000/100 * account balance (owner object)
 */
 typedef struct _distribution_lists_window DistributionListsWindow;
 
-/** @struct DistributionListsNotebook
+
+/** @struct NewDistributionList
+
+@param  GtkWidget*  dialog - Pointer to the dialog.
+@param  GtkWidget*  entry_description - The description of the distribution list dialog.
+@param  GtkWidget*  entry_name - The name of the given distribution list.
+@param  DistributionListWindow* distriblist_window - Pointer to the distribution list window.
+@param  DistributionListNotebook notebook - Pointer to the notebook entry.
+@param  DistributionListOwners owners - Pointer to the owner list.
+@param  GncDistributionList* this_distriblist - Copy of the current distribution list pointer.
+*/
+typedef struct _new_distribution_list NewDistributionList;
+
+/** @struct DistributionListNotebook
 
 @param  GtkWidget*  dialog - Pointer to the notebook dialog.
 @param  GtkWidget*  notebook - Pointer to the notebook entry.
 @param  GtkWidget*  parent - Pointer to the parent of the notebook dialog.
 */
-typedef struct _distribution_lists_notebook DistributionListsNotebook;
+typedef struct _distribution_list_notebook DistributionListNotebook;
 
-/** @struct DistributionListsOwners
+/** @struct DistributionListOwners
 
+@param  GtkWidget*  acct_tree - Pointer to the account tree.
+@param  GtkTreeView*  account_view - Pointer to tree view for given accounts.
+@param  GtkListStore*  account_store - Pointer to the store offering valid account types.
 @param  GtkWidget*  dialog - Pointer to the owners dialog.
 @param  GtkWidget*  owners - Pointer to the owners entry.
 @param  GtkWidget*  parent - Pointer to the parent of the owners dialog.
+@param  GList*  owner_accout_types - Pointer to the list of owner account types.
+@param  GList*  owners - Pointer to the list of assigned owners.
+@param  DistributionListType* type - Pointer to the type of the given owner.
 */
-typedef struct _distribution_lists_owners DistributionListsOwners;
+typedef struct _distribution_list_owners DistributionListOwners;
 
 #include "qof.h"
 
@@ -153,7 +172,8 @@ owners_assign_cb (
 void
 owners_edit_cb (
     GtkButton *button,
-    DistributionListsWindow *distriblists_window);
+    DistributionListsWindow *distriblists_window,
+    NewDistributionList *new_distriblist);
 void owners_ok_cb (
     GtkButton *button,
     DistributionListsWindow *distriblists_window);
